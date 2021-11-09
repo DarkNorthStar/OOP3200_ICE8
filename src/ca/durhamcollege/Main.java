ece929ad8abe151649c2d9f35d24e2cdec76f34b
Tyler Osborne <tyler.osborne@dcmail.ca>
2021-11-09 17:41:31 -0500

    Added Basic Validation

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -6,6 +6,7 @@
 
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -23,11 +24,34 @@ public class Main
         // Input
         System.out.print("Please enter your name: ");
         name = scanner.nextLine();
-        System.out.print("Please enter your age: ");
-        age = scanner.nextFloat();
-        System.out.println();
+
+        boolean validInput = false;
+        while (validInput == false)
+        {
+            System.out.print("Please enter your age: ");
+            try
+            {
+                age = scanner.nextFloat();
+                validInput = true;
+
+                if((age <= 0.0f) || (age >= 120.0))
+                {
+                    System.out.printf("ERROR: You must enter an age greater then 0.0 and less then 120.0\n");
+                    scanner.nextLine();
+                    validInput = false;
+                }
+            }
+            catch(InputMismatchException inputMismatchException)
+            {
+                System.out.printf("ERROR: You must enter a valid floating-point number\n");
+                scanner.nextLine();
+                validInput = false;
+            }
+
+        }
 
         // Output
+        System.out.println();
         System.out.printf("You Entered: %s \n",name);
         System.out.printf("You Entered: %.2f \n",age);
 
