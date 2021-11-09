59f4e2bdaea3a34aca507bb15cade32b21b5ec65
Tyler Osborne <tyler.osborne@dcmail.ca>
2021-11-09 16:56:43 -0500

    Basic Input Output 2 Variables

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -13,11 +13,23 @@ public class Main
 
     public static void main(String[] args)
     {
-        String name = "";
+        // Variables
+        String name = null;
+        float age = 0.0f;
+
+        // Scanner
         Scanner scanner = new Scanner(System.in);
+
+        // Input
         System.out.print("Please enter your name: ");
         name = scanner.nextLine();
-        System.out.printf("You Entered: %s",name);
+        System.out.print("Please enter your age: ");
+        age = scanner.nextFloat();
+        System.out.println();
+
+        // Output
+        System.out.printf("You Entered: %s \n",name);
+        System.out.printf("You Entered: %.2f \n",age);
 
     }
 }
