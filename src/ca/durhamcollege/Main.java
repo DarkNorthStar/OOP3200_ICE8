e43f9d4832cd34b1d295444920b4833b25c8d7b6
Tyler Osborne <tyler.osborne@dcmail.ca>
2021-11-09 16:49:07 -0500

    Basic Input Output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,10 +1,23 @@
+/*
+*   Author: Tyler Osborne
+*   Date: 2021-11-09
+ */
+
+
 package ca.durhamcollege;
+
+import java.util.Scanner;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
+        String name = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = scanner.nextLine();
+        System.out.printf("You Entered: %s",name);
 
     }
 }
